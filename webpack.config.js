56dd01eef8144eed9471f673c8b2298b10fb616c
Richard Rino <richardvrino@gmail.com>
2023-03-07 19:44:42 -0600

    Create initial files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    },
+    port: 3000,
+    open: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
